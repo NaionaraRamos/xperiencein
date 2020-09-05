51b76bb953deb662eff072e22abfc6a6b77f0301
NaionaraRamos <38890289+NaionaraRamos@users.noreply.github.com>
2020-09-04 21:44:12 -0300

    Update settings.py

--- experiencein/settings.py
+++ experiencein/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'experiencein.wsgi.application'
 
 DATABASES = {
     'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	'ENGINE': 'django.db.backends.mysql',
-	'NAME': 'experiencein',
-	'USER': 'root',
-	'PASSWORD': 'senha',
-	'HOST': '127.0.0.1',
-	'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#	'ENGINE': 'django.db.backends.mysql',
+#	'NAME': 'experiencein',
+#	'USER': 'root',
+#	'PASSWORD': '',
+#	'HOST': '127.0.0.1',
+#	'PORT': '3306'
     }
 }
 
